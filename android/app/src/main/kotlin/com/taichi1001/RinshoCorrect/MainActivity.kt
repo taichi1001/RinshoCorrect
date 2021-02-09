12eaf36a96beff022d27cc1b5335e3a891a3fe6c
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2021-02-09 15:03:17 +0900

    スクショ対応

--- android/app/src/main/kotlin/com/taichi1001/RinshoCorrect/MainActivity.kt
+++ android/app/src/main/kotlin/com/taichi1001/RinshoCorrect/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.taichi1001.template
 
 import io.flutter.embedding.android.FlutterActivity
+import android.view.WindowManager.LayoutParams;
 
 class MainActivity: FlutterActivity() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(LayoutParams.FLAG_SECURE); 
+    }
 }
